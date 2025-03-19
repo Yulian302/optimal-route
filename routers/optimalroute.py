784eb58f2a87c420f6448e289db8c8129e5c27b4
Yulian302 <bohomolyulian3022003@gmail.com>
2025-03-19 11:37:14 -0400

    (bug): Resolve index out of bounds when k=N-1

--- routers/optimalroute.py
+++ routers/optimalroute.py
@@ -17,5 +17,5 @@ async def get_optimal_route(start_address: str = Form(...), n_best: int = Form(.
     _, min_cost, best_path = best_k_city_tsp(adj_matrix, n_best, 0)
     minutes, seconds = divmod(min_cost, 60)
     best_path_verbose = " -> ".join([start_address if node ==
-                                     0 else addresses[node] for node in best_path])+" -> "+start_address
+                                     0 else addresses[node-1] for node in best_path])+" -> "+start_address
     return {"Minimum travel time": f"{minutes:.0f} minutes {seconds:.0f} seconds", "Optimal path": best_path_verbose}
